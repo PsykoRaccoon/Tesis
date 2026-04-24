ef8430105e845e202f5d182bc6883a9f04030080
Victor Sanchez <vickoss9004@gmail.com>
2026-04-24 00:00:45 -0600

    Prefabs de lasers
    Agregue un sistema donde dependiendo de con que choque el laser, se puede crear un prefab especial para cada tipo de superficie con lo que choque.

---
+++ Assets/Editor/LayerDrawer.cs
@@ -0,0 +1,13 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(LayerAttribute))]
+public class LayerDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        property.intValue = EditorGUI.LayerField(position, label, property.intValue);
+    }
+}
+#endif
--- Assets/Scripts/Habilidades/Player 1/LaserAbility.cs
+++ Assets/Scripts/Habilidades/Player 1/LaserAbility.cs
@@ -1,21 +1,36 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class LaserAbility : MonoBehaviour
 {
     [Header("Config")]
     [SerializeField] private float maxDistance;
     [SerializeField] private LayerMask hitLayers;
+    [SerializeField] private LayerMask effectOnlyLayers;
+
+    [Header("Impact Effects")]
+    [SerializeField] private List<LaserImpactEntry> impactEffects;
+    
 
     private BoxCollider boxCollider;
-    private RaycastHit hitInfo;
+    private float currentLaserLength = 0;
+
+    private GameObject currentEffect;
+    private int currentHitLayer = -1;
+
+    [System.Serializable]
+    public class LaserImpactEntry
+    {
+        public string label;           
+        [Layer] public int layer;      
+        public GameObject prefab;     
+    }
 
     private void Awake()
     {
         boxCollider = GetComponent<BoxCollider>();
-        currentLaserLength=maxDistance;
+        currentLaserLength = maxDistance;
     }
-
-    private float currentLaserLength = 0;
 
     private void LateUpdate()
     {
@@ -23,9 +38,23 @@ public class LaserAbility : MonoBehaviour
         Vector3 direction = transform.forward;
 
         if (Physics.Raycast(origin, direction, out RaycastHit hitInfo, maxDistance, hitLayers, QueryTriggerInteraction.Ignore))
+        {
             currentLaserLength = hitInfo.distance;
+            HandleImpactEffect(hitInfo);
+        }
         else
+        {
             currentLaserLength = maxDistance;
+
+            if (Physics.Raycast(origin, direction, out RaycastHit effectHitInfo, maxDistance, effectOnlyLayers, QueryTriggerInteraction.Collide))
+            {
+                HandleImpactEffect(effectHitInfo);
+            }
+            else
+            {
+                ClearImpactEffect();
+            }
+        }
 
         Debug.DrawRay(origin, direction * currentLaserLength, Color.red);
     }
@@ -41,8 +70,53 @@ public class LaserAbility : MonoBehaviour
         boxCollider.size = new Vector3(0.5f, 0.5f, length);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void HandleImpactEffect(RaycastHit hitInfo)
     {
-        Debug.Log("rasho laser impacto con: " + other.name + " | Tag: " + other.tag);
+        int hitLayer = hitInfo.collider.gameObject.layer;
+        string hitLayerName = LayerMask.LayerToName(hitLayer);
+
+        if (hitLayer != currentHitLayer)
+        {
+            ClearImpactEffect();
+            currentHitLayer = hitLayer;
+
+            GameObject prefab = GetPrefabForLayer(hitLayer);
+            if (prefab != null)
+            {
+                currentEffect = Instantiate(prefab, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+                Debug.Log($"[Laser] Impacto con: {hitInfo.collider.name} | Layer: {hitLayerName} | Prefab instanciado: {prefab.name}");
+            }
+            else
+            {
+                Debug.LogWarning($"[Laser] Impacto con: {hitInfo.collider.name} | Layer: {hitLayerName} | Sin prefab asignado para este layer");
+            }
+        }
+    }
+
+    private void ClearImpactEffect()
+    {
+        if (currentEffect != null)
+        {
+            Debug.Log($"[Laser] Efecto limpiado: {currentEffect.name}");
+            Destroy(currentEffect);
+            currentEffect = null;
+        }
+        currentHitLayer = -1;
+    }
+
+    private GameObject GetPrefabForLayer(int layer)
+    {
+        foreach (var entry in impactEffects)
+        {
+            if (entry.layer == layer)
+                return entry.prefab;
+        }
+        return null;
+    }
+
+    private void OnDisable()
+    {
+        ClearImpactEffect();
     }
 }
+
